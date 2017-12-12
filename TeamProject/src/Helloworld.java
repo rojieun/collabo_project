259e512a69a17c35ffbf7873cfb4ab2dc1e90fa4
roji <wldms9632@naver.com>
2017-12-12 15:35:29 +0900

    출력문 계속 추가

--- TeamProject/src/Helloworld.java
+++ TeamProject/src/Helloworld.java
@@ -4,6 +4,7 @@ public class Helloworld {
 	public static void main(String[] args) {
 		System.out.println("test중");
 		System.out.println("한글test");
+		System.out.println("계정 로그인 자동저장");
+	
 	}
-
 }
